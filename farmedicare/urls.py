896be0153a0ccf6e75d12bf46851c6ca7423fbdc
PabloDomiciano <pablo.domiciano2019@gmail.com>
2025-04-25 10:31:35 -0300

    Adicionando parte da tela inicial

--- farmedicare/urls.py
+++ farmedicare/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('paginas.urls'))
+    path('paginas/', include('paginas.urls'))
 ]

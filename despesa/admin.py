1e5a18f9a269cc35e7b074b27b107ecb824ac9ec
PabloDomiciano <pablo.domiciano2019@gmail.com>
2025-06-18 12:00:15 -0300

    feat: estrutura inicial do módulo despesas (models, views e urls)

--- despesa/admin.py
+++ despesa/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from despesa.models import CategoriaDespesa, Despesa, ParcelaDespesa
+
 # Register your models here.
+
+admin.site.register(Despesa)
+admin.site.register(ParcelaDespesa)
+admin.site.register(CategoriaDespesa)
